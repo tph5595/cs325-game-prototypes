49d974d024bf72df0b163c880cd492f06fc5c739
Taylor Henderson <tph5595@verizon.net>
2018-02-08 11:23:13 -0500

    5

--- game1/js/main.js
+++ game1/js/main.js
@@ -61,6 +61,6 @@ window.onload = function() {
     // in X or Y.
     // This function returns the rotation angle that makes it visually match its
     // new trajectory.
-    bouncy.rotation = game.physics.arcade.accelerateToPointer(bouncy, game.input.activePointer, 10, 500, 500);
+    bouncy.rotation = game.physics.arcade.accelerateToPointer(bouncy, game.input.activePointer, 50, 500, 500);
   }
 };
