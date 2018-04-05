dddf6b3a6a6548c833aef0a0027b59807847d941
Taylor Henderson <tph5595@verizon.net>
2018-04-05 03:14:14 -0400

    Game working

--- game6/js/Game.js
+++ game6/js/Game.js
@@ -56,26 +56,33 @@ BasicGame.Game.prototype = {
 
     this.add.sprite(0, 0, 'gameback');
 
-    this.scoreText = this.add.text(600, 510, 'Acorns: 500');
+    this.scoreText = this.add.text(600, 530, 'Acorns: 500');
     this.scoreText.font = 'VT323';
     this.scoreText.fontSize = 30;
     this.scoreText.fill = "#ffffff";
     this.scoreText.align = "left";
     this.score = 500;
 
-    this.guessText = this.add.text(400, 510, 'Guess: 1');
+    this.guessText = this.add.text(350, 530, 'Guess: 1');
     this.guessText.font = 'VT323';
     this.guessText.fontSize = 30;
     this.guessText.fill = "#ffffff";
     this.guessText.align = "left";
     this.guessValue = 1;
 
-    this.realText = this.add.text(200, 510, 'Real Value: ???');
+    this.realText = this.add.text(50, 530, 'Real Value: ???');
     this.realText.font = 'VT323';
     this.realText.fontSize = 30;
     this.realText.fill = "#ffffff";
     this.realText.align = "left";
     this.realValue = -1;
+
+    this.XText = this.add.text(375, 125, 'X');
+    this.XText.font = 'VT323';
+    this.XText.fontSize = 75;
+    this.XText.fill = "#000000";
+    this.XText.align = "left";
+    this.XText.alpha = 0;
 
     this.cursors = this.input.keyboard.createCursorKeys();
 
@@ -86,71 +93,71 @@ BasicGame.Game.prototype = {
     this.roundOver = false;
     this.buttonDown = false;
 
-    this.m10 = this.add.sprite(150, 150, 'acorn');
-    this.m11 = this.add.sprite(150, 150, 'acorn');
-    this.m12 = this.add.sprite(150, 150, 'acorn');
-    this.m13 = this.add.sprite(150, 150, 'acorn');
+    this.m10 = this.add.sprite(150, 100, 'acorn');
+    this.m11 = this.add.sprite(200, 100, 'acorn');
+    this.m12 = this.add.sprite(250, 100, 'acorn');
+    this.m13 = this.add.sprite(300, 100, 'acorn');
     this.m14 = this.add.sprite(150, 150, 'acorn');
-    this.m15 = this.add.sprite(150, 150, 'acorn');
-    this.m16 = this.add.sprite(150, 150, 'acorn');
-    this.m17 = this.add.sprite(150, 150, 'acorn');
-    this.m18 = this.add.sprite(150, 150, 'acorn');
-    this.m19 = this.add.sprite(150, 150, 'acorn');
+    this.m15 = this.add.sprite(200, 150, 'acorn');
+    this.m16 = this.add.sprite(250, 150, 'acorn');
+    this.m17 = this.add.sprite(300, 150, 'acorn');
+    this.m18 = this.add.sprite(200, 200, 'acorn');
+    this.m19 = this.add.sprite(250, 200, 'acorn');
 
-    this.m20 = this.add.sprite(150, 150, 'acorn');
-    this.m21 = this.add.sprite(150, 150, 'acorn');
-    this.m22 = this.add.sprite(150, 150, 'acorn');
-    this.m23 = this.add.sprite(150, 150, 'acorn');
-    this.m24 = this.add.sprite(150, 150, 'acorn');
-    this.m25 = this.add.sprite(150, 150, 'acorn');
-    this.m26 = this.add.sprite(150, 150, 'acorn');
-    this.m27 = this.add.sprite(150, 150, 'acorn');
-    this.m28 = this.add.sprite(150, 150, 'acorn');
-    this.m29 = this.add.sprite(150, 150, 'acorn');
+    this.m20 = this.add.sprite(400 + 50, 100, 'acorn');
+    this.m21 = this.add.sprite(450 + 50, 100, 'acorn');
+    this.m22 = this.add.sprite(500 + 50, 100, 'acorn');
+    this.m23 = this.add.sprite(550 + 50, 100, 'acorn');
+    this.m24 = this.add.sprite(400 + 50, 150, 'acorn');
+    this.m25 = this.add.sprite(450 + 50, 150, 'acorn');
+    this.m26 = this.add.sprite(500 + 50, 150, 'acorn');
+    this.m27 = this.add.sprite(550 + 50, 150, 'acorn');
+    this.m28 = this.add.sprite(450 + 50, 200, 'acorn');
+    this.m29 = this.add.sprite(500 + 50, 200, 'acorn');
 
-    this.m10.alpha = 1;
-    this.m11.alpha = 1;
-    this.m12.alpha = 1;
-    this.m13.alpha = 1;
-    this.m14.alpha = 1;
-    this.m15.alpha = 1;
-    this.m16.alpha = 1;
-    this.m17.alpha = 1;
-    this.m18.alpha = 1;
-    this.m19.alpha = 1;
+    this.m10.alpha = 0;
+    this.m11.alpha = 0;
+    this.m12.alpha = 0;
+    this.m13.alpha = 0;
+    this.m14.alpha = 0;
+    this.m15.alpha = 0;
+    this.m16.alpha = 0;
+    this.m17.alpha = 0;
+    this.m18.alpha = 0;
+    this.m19.alpha = 0;
 
-    this.m20.alpha = 1;
-    this.m21.alpha = 1;
-    this.m22.alpha = 1;
-    this.m23.alpha = 1;
-    this.m24.alpha = 1;
-    this.m25.alpha = 1;
-    this.m26.alpha = 1;
-    this.m27.alpha = 1;
-    this.m28.alpha = 1;
-    this.m29.alpha = 1;
+    this.m20.alpha = 0;
+    this.m21.alpha = 0;
+    this.m22.alpha = 0;
+    this.m23.alpha = 0;
+    this.m24.alpha = 0;
+    this.m25.alpha = 0;
+    this.m26.alpha = 0;
+    this.m27.alpha = 0;
+    this.m28.alpha = 0;
+    this.m29.alpha = 0;
 
-    this.s0 = this.add.sprite(300, 300, 's1');
-    this.s1 = this.add.sprite(300, 300, 's2');
-    this.s2 = this.add.sprite(300, 300, 's3');
-    this.s3 = this.add.sprite(300, 300, 's4');
-    this.s4 = this.add.sprite(300, 300, 's5');
-    this.s5 = this.add.sprite(300, 300, 's1');
-    this.s6 = this.add.sprite(300, 300, 's2');
-    this.s7 = this.add.sprite(300, 300, 's3');
-    this.s8 = this.add.sprite(300, 300, 's4');
-    this.s9 = this.add.sprite(300, 300, 's5');
+    this.s0 = this.add.sprite(375, 450, 's1');
+    this.s1 = this.add.sprite(675, 300, 's2');
+    this.s2 = this.add.sprite(525, 300, 's3');
+    this.s3 = this.add.sprite(225, 350, 's4');
+    this.s4 = this.add.sprite(75, 300, 's5');
+    this.s5 = this.add.sprite(500, 400, 's1');
+    this.s6 = this.add.sprite(200, 450, 's2');
+    this.s7 = this.add.sprite(350, 300, 's3');
+    this.s8 = this.add.sprite(100, 400, 's4');
+    this.s9 = this.add.sprite(700, 450, 's5');
 
-    this.s0.alpha = 1;
-    this.s1.alpha = 1;
-    this.s2.alpha = 1;
-    this.s3.alpha = 1;
-    this.s4.alpha = 1;
-    this.s5.alpha = 1;
-    this.s6.alpha = 1;
-    this.s7.alpha = 1;
-    this.s8.alpha = 1;
-    this.s9.alpha = 1;
+    this.s0.alpha = 0;
+    this.s1.alpha = 0;
+    this.s2.alpha = 0;
+    this.s3.alpha = 0;
+    this.s4.alpha = 0;
+    this.s5.alpha = 0;
+    this.s6.alpha = 0;
+    this.s7.alpha = 0;
+    this.s8.alpha = 0;
+    this.s9.alpha = 0;
   },
 
   update: function() {
@@ -201,13 +208,18 @@ BasicGame.Game.prototype = {
     var r = Math.abs(this.realValue - this.guessValue) / 10.0;
     r = 10 - r;
     r += Math.floor(Math.random() * 4) - 2;
+    if (r > 10) {
+      r = 10;
+    }else if (r < 1) {
+      r = 1;
+    }
     while (r > 0) {
       this.showRand();
       r--;
     }
     setTimeout(function() {
-      this.hideSq();
-    }, 1500);
+      mygame.hideSq();
+    }, 500);
   },
 
   betReveal: function() {
@@ -238,9 +250,10 @@ BasicGame.Game.prototype = {
 
     this.showMul1();
     this.showMul2();
+    this.XText.alpha = 1;
     setTimeout(function() {
-      this.clearMul();
-    }, 1500);
+      mygame.clearMul();
+    }, 800);
   },
 
   showMul1: function() {
@@ -282,7 +295,7 @@ BasicGame.Game.prototype = {
         this.m18.alpha = 1;
       } else if (rand == 9 && this.m19.alpha == 0) {
         this.m19.alpha = 1;
-      } else {
+      } else{
         good = false;
       }
     }
@@ -320,6 +333,8 @@ BasicGame.Game.prototype = {
   },
 
   clearMul: function() {
+    this.XText.alpha = 0;
+
     this.m10.alpha = 0;
     this.m11.alpha = 0;
     this.m12.alpha = 0;
@@ -405,7 +420,6 @@ BasicGame.Game.prototype = {
 
     var go = this.add.sprite(-75, 0, 'gameover');
     go.alpha = 0;
-    this.timer.alpha = 0;
 
     this.add.tween(go).to({
       alpha: 1
@@ -427,9 +441,8 @@ BasicGame.Game.prototype = {
   gameWin: function() {
     this.playingG = false;
 
-    var gw = this.add.sprite(-75, 0, 'gameWin');
+    var gw = this.add.sprite(50, 50, 'gamewin');
     gw.alpha = 0;
-    this.timer.alpha = 0;
 
     this.add.tween(gw).to({
       alpha: 1
@@ -439,7 +452,7 @@ BasicGame.Game.prototype = {
 
     this.music.fadeOut(1000);
 
-    var butt = this.add.button(this.world.centerX - 95, 450, 'restartButton', this.restart, this, 2, 1, 0);
+    var butt = this.add.button(this.world.centerX - 95, 400, 'restartButton', this.restart, this, 2, 1, 0);
 
     butt.alpha = 0;
 
